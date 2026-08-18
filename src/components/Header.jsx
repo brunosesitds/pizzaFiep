71521139ca66fd302300daa6e23b3ebdfa71929e
brunosesitds <brunosesitds@gmail.com>
2026-08-18 09:42:34 -0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Header(){
+    const [ menuAtivo, setMenuAtivo] = useState(false);
+
+    const toggleMenu = () =>{
+        setMenuAtivo(!menuAtivo);''
+    };
+
+    return(
+        <>
+            <header className="header">
+                <h1 className="logo">Pizza Fiep</h1>
+                
+                {/* Quando clica, chama a função toggleMenu */}
+                <div className="meu-toggle" onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </header>
+        </>
+    )
+}
